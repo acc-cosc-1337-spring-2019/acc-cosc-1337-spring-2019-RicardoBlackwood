954b4ac89e2b3908612cb360584f59852d2a63a6
Ricardo Blackwood <ricardo.blackwood@g.austincc.edu>
2019-01-31 19:27:43 -0600

    generation examples

--- examples/01_module/06_if_else/if_else.cpp
+++ examples/01_module/06_if_else/if_else.cpp
@@ -13,5 +13,30 @@
 
 std::string get_generation(int year)
 {
+	{
+		if (year = 1996 && year <= 2014)
+		{
+			return "Centenial";
+		}
+		else if (year >= 1977 && year <= 1995)
+		{
+			return "Millenial";
+		}
+		else if (year >= 1965 && year <= 1976)
+		{
+			return "Generation X";
+		}
+		else if (year >= 1946 && year <= 1964)
+		{
+			return "Baby Boomer";
+		}
+		else if (year >= 1925 && year <= 1945)
+		{
+			return "Silent Generation";
+		}
+		else
+		{
+			return "invalid Year";
+		}
 	return std::string();
 }
